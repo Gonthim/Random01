7b8cb0c12b924bfa58c2a9d55bf5b2c5a5be9205
Joe Wertz <Joe Wertz@JoeLTP>
2020-05-10 20:35:56 -0600

    Tab/Space formatting

--- src/main/java/random/Main.java
+++ src/main/java/random/Main.java
@@ -16,25 +16,25 @@ public class Main extends Application {
 
 	@Override
 	public void start(Stage stage) throws Exception {
-		
+
 		// Scene Setup
-    Group group = new Group();
-     
-    Scene scene = new Scene(group ,600, 300);
-    
-    scene.setFill(Color.BROWN);
-    
-    stage.setTitle("Sample Application");
- 
-    stage.setScene(scene);
-    
-    stage.show();
-    
-    // Sprite Test    
-    Image sprite = new Image( getClass().getResource("/pacman.png").toExternalForm() );
-    ImageView imageView = new ImageView(sprite);
-    group.getChildren().add(imageView);
-  
+		Group group = new Group();
+
+		Scene scene = new Scene(group, 600, 300);
+
+		scene.setFill(Color.BROWN);
+
+		stage.setTitle("Sample Application");
+
+		stage.setScene(scene);
+
+		stage.show();
+
+		// Sprite Test
+		Image sprite = new Image(getClass().getResource("/pacman.png").toExternalForm());
+		ImageView imageView = new ImageView(sprite);
+		group.getChildren().add(imageView);
+
 	}
 
 }
